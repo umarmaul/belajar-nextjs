e4297c5e489483b722de99b7064f71aa85386b1d
umarmaul <umarmaulana31@gmail.com>
2024-06-13 17:26:15 +0700

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function Layout({ children }) {
+	return (
+		<html lang="en">
+			<head>
+				<title>Belajar Next.js Fundamental</title>
+			</head>
+			<body>{children}</body>
+		</html>
+	);
+}
