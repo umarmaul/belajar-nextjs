97be7e2cc25f3f11b7b008f055bba373e172e11b
umarmaul <umarmaulana31@gmail.com>
2024-06-14 14:10:46 +0700

    persiapan strapi

---
+++ scripts/strapi-request.mjs
@@ -0,0 +1,33 @@
+import { writeFileSync } from "node:fs";
+import qs from "qs";
+
+const url =
+	"http://localhost:1337/api/posts" +
+	"?" +
+	qs.stringify(
+		{
+			fields: [
+				"title",
+				"slug",
+				"description",
+				"publishedAt",
+				"author",
+				"body",
+			],
+			populate: {
+				image: {
+					fields: ["url"],
+				},
+			},
+			sort: ["publishedAt:desc"],
+			pagination: { pageSize: 3 },
+		},
+		{ encodeValuesOnly: true }
+	);
+
+const response = await fetch(url);
+const body = await response.json();
+const posts = JSON.stringify(body, null, 2);
+
+const file = "scripts/strapi-response.json";
+writeFileSync(file, posts, "utf8");
